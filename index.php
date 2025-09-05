0cdb2af26f5c80b5564e9db5ce74374448fce58a
Sergey Sidorkin <sergei.sidorkin@tptlive.ee>
2025-09-05 10:28:07 +0300

    Add math examples

--- index.php
+++ index.php
@@ -1,40 +1,21 @@
 <?php
 
-$array = array(1, 2, 3); // old way
+$num = 1;
 
-var_dump($array);
-
-$array = [1, 2, 3]; // new way
-
-var_dump($array);
-
-$array = [
-    1,
-    'Hello',
-    true,
-    [1, 2, 3],
-    3.4,
-];
-var_dump($array);
-
-$array = [
-    1,
-    true,
-    'name' => 'Sergey',
-    'age' => '17',
-    'isMale' => true,
-    100 => 4,
-    5,
-    6.3,
-    NULL,
-];
-var_dump($array);
-
-var_dump($array[1]);
-var_dump($array['name']);
-$array = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
-var_dump($array[2][1]);
+$num = $num + 1;
+$num = $num - 1;
+$num = $num * 2;
+$num = $num / 2;
+$num = 7; //
+$num = $num % 2;
+$num = 14; //
+$num = $num % 4;
+var_dump($num);
+$num += 1;
+$num -+ 1;
+$num += 2;
+$num /= 2;
+$num %=2;
+$num == 2;
+$num++;
+$num--;
